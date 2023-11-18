f89554a8c094f506a5161e46adb021c02251d369
Andrey <priv1984@yandex.ru>
2023-11-19 00:40:09 +0700

    install react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {useState} from 'react'
 import './App.css'
+import {Component} from "./useStateExample/Component";
 
 function App() {
     const [count, setCount] = useState(1)
 
     return <>
         <h1>Vite + React</h1>
+        <Component/>
         <div className="card">
             <button onClick={() => setCount((count) => count + 1)} onContextMenu={
                 (e) => {
---
+++ src/useStateExample/Component.jsx
@@ -0,0 +1,22 @@
+//import {Button} from 'react-bootstrap';
+import {Card} from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+
+export function Component() {
+
+
+    return <>
+        <div>
+            <h1> What in React? </h1>
+            <Button>
+                456
+            </Button>
+            <Card>
+                <Card.Body>A JavaScript library for building</Card.Body>
+            </Card>
+
+
+        </div>
+    </>
+
+}

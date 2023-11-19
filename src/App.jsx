e2e2bdfd19cdd5a0a3981c64233f8c0ec9f8a590
Andrey <priv1984@yandex.ru>
2023-11-20 00:54:08 +0700

    react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import {useState} from 'react'
-import './App.css'
+//import './App.css'
 import {Component} from "./useStateExample/Component";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    const [count, setCount] = useState(1)
 
-    return <>
-        <h1>Vite + React</h1>
-        <Component/>
-        <div className="card">
-            <button onClick={() => setCount((count) => count + 1)} onContextMenu={
-                (e) => {
-                    setCount((count) => count - 1)
-                    e.preventDefault();
-                    e.stopPropagation();
-                }
-            }>
-                count is {count}
-            </button>
-        </div>
+
+    return       <>
+        <Component/>{' '}
     </>
 }
 
--- src/useStateExample/Component.jsx
+++ src/useStateExample/Component.jsx
@@ -1,22 +1,41 @@
-//import {Button} from 'react-bootstrap';
+
+import {Button} from 'react-bootstrap';
 import {Card} from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
+import {useState} from "react";
 
 export function Component() {
 
-
+    const [count, setCount] = useState(1)
+    const [variant, setVariant] = useState("primary")
     return <>
-        <div>
             <h1> What in React? </h1>
-            <Button>
-                456
-            </Button>
+
             <Card>
                 <Card.Body>A JavaScript library for building</Card.Body>
             </Card>
-
-
-        </div>
+            <Button variant={variant} onClick={() => {
+                setCount((count) => count + 1)
+                setVariant(() => "primary")
+            }} onContextMenu={
+                (e) => {
+                    setCount((count) => count - 1)
+                    setVariant(() => "secondary")
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            }>
+                count is {count}
+            </Button>{' '}
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+        <Button variant="warning">Warning</Button>{' '}
+        <Button variant="danger">Danger</Button>{' '}
+        <Button variant="info">Info</Button>{' '}
+        <Button variant="light">Light</Button>{' '}
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
     </>
 
 }
